dc6d984c5dc6067d88c1522cbbcf0380387e90b3
mSolomonGames <michaelsolomon.games@gmail.com>
2020-03-17 09:02:04 +0200

    Updates

---
+++ precache-manifest.652e1f82e129ab30cc2d520cc4cf882d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61813ec6c135610fd3710adce0ec207",
+    "url": "/OrderFormReact/index.html"
+  },
+  {
+    "revision": "1298d2316696175efb51",
+    "url": "/OrderFormReact/static/css/main.8bce8e2f.chunk.css"
+  },
+  {
+    "revision": "a50518afaa4a185fb285",
+    "url": "/OrderFormReact/static/js/2.a861849d.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/OrderFormReact/static/js/2.a861849d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1298d2316696175efb51",
+    "url": "/OrderFormReact/static/js/main.bd41494d.chunk.js"
+  },
+  {
+    "revision": "61fa186b39fff05802b2",
+    "url": "/OrderFormReact/static/js/runtime-main.54298d77.js"
+  },
+  {
+    "revision": "4035fb515f8356661dc739a085b99782",
+    "url": "/OrderFormReact/static/media/Logo.4035fb51.png"
+  }
+]);
